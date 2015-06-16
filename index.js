c0a10e65e996c60ee847fab35e7f4ba46801f486
Michael Gordon <mike1gordon@gmail.com>
2015-06-16 18:43:15 +0200

    Added Query user by id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var _ = require('lodash')
 	, app = express()
 
 app.get('/api/tweets', function (req, res) {
-	if(!req.query.userId) {
+	if (!req.query.userId) {
 		return res.sendStatus(400)
 	}
 
@@ -12,6 +12,16 @@ app.get('/api/tweets', function (req, res) {
 	var sortedTweets = tweets.sort(function (a,b) {return b.created - a.created })
 
 	res.send({ tweets: sortedTweets })
+})
+
+app.get('/api/users/:userId', function(req, res) {
+  var user = _.find(fixtures.users, 'id', req.params.userId)
+
+  if (!user) {
+    return res.sendStatus(404)
+  }
+
+  res.send({ user: user })
 })
 
 var server = app.listen(3000, '127.0.0.1')
